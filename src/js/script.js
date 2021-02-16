011bf8f2f58541b52040eacfe7b348e7d8f5a1d4
Vladyslav <notkiller999@gmail.com>
2021-02-16 09:32:03 +0200

    privacy policy
    add agreement with the privacy policy and privacy policy page aaaand scroll button

--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,46 @@ const percents = document.querySelectorAll('.using__skills-percent'),
 percents.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+(function() {
+  'use strict';
+
+  function trackScroll() {
+    var scrolled = window.pageYOffset;
+    var coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+      goTopBtn.classList.add('scroll_up_active');
+    }
+    if (scrolled < coords) {
+      goTopBtn.classList.remove('scroll_up_active');
+    }
+  }
+
+  function backToTop() {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -80);
+      setTimeout(backToTop, 0);
+    }
+  }
+
+  var goTopBtn = document.querySelector('.scroll_up');
+
+  window.addEventListener('scroll', trackScroll);
+  goTopBtn.addEventListener('click', backToTop);
+})();
